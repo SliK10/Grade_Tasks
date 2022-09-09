4d0d9d73886e4e0891613c1651bc4dbebec5dbb3
Vyacheslav Kaskov <Dr.Sfar@yandex.ru>
2022-09-09 13:55:12 +0300

    Solved task 8

---
+++ Task8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+
+Type myMatch = typeof(MyMatch);
+
+MyMatch? match = (MyMatch?)(myMatch.GetConstructors()?.FirstOrDefault()?.Invoke(parameters: null));
+
+var res = match?.Sum(1, 2);
+Console.WriteLine(res);
+
+public class MyMatch
+{
+	public int Sum(int a, int b)
+	{
+		return a + b;
+	}
+
+	public int Sum(int a, int b, int c)
+	{
+		return a + b + c;
+	}
+
+	public int Sum(int a, int b, int c, int d)
+	{
+		return a + b + c + d;
+	}
+}
